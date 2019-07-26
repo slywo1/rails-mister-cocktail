953c71d53ecb2d4933e7e743fa205ec14001b77a
Waiyan Oo <slywo1@nottingham.edu.cn>
2019-07-26 12:40:24 +0800

    mister cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'cocktails#index'
+
+  resources :cocktails, only: [:new, :create, :index, :show] do
+    resources :doses, shallow: true, except: [:index, :edit, :update, :show]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+if Rails.env.development?
+  Cocktail.destroy_all
+  Dose.destroy_all
+  Ingredient.destroy_all
+end
+
+url = "https://raw.githubusercontent.com/maltyeva/iba-cocktails/master/recipes.json"
+opened_url = open(url).read
+parsed_url = JSON.parse(opened_url)
+
+parsed_url.each do |cocktail|
+  c = Cocktail.create!(name: cocktail["name"])
+  cocktail["ingredients"].each do |ingredient|
+    if ingredient["ingredient"]
+      i = Ingredient.find_or_create_by(name: ingredient["label"].present? ? ingredient["label"] : ingredient["ingredient"])
+      d = Dose.create!(description: ingredient["amount"].to_s + " " + ingredient["unit"], cocktail: c, ingredient: i)
+    end
+  end
+end
